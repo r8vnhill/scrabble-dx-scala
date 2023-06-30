a48076989ade3facf17ee01344c9be23d8c271a4
r8vnhill <reachme@ravenhill.cl>
2023-06-30 16:11:16 -0400

    DOC Manually created case class vs native implementation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,14 @@ import expr.values.{Integer, Real}
 import cl.ravenhill.scrabble.dx.matcher.ExpressionPrinter
 import cl.ravenhill.scrabble.dx.visitor.PrintVisitor
 
+class A(var a: Int)
+object A {
+  def unapply(a: A): Option[Int] = {
+    a.a = a.a + 1
+    Some(a.a)
+  }
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
     val expression = Add(Multiply(Real(1.5), Integer(2)), Integer(3))
