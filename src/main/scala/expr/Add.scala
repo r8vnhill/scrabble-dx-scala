fa110d3a3be4897c537d3d9c6b8e1b417a5562a4
R8V <ignacio.slater@ug.uchile.cl>
2023-11-24 14:38:55 -0300

    Add Add and Multiply classes in expr package
    Added Add and Multiply classes to handle addition and multiplication operations. These classes take two Expressions as input and extend the Expression trait. This will enhance readability of the code and allows for easy showcasing of operations between two values.

---
+++ src/main/scala/expr/Add.scala
@@ -0,0 +1,4 @@
+package cl.ravenhill.scrabble.dx
+package expr
+
+class Add(val left: Expression, val right: Expression) extends Expression
---
+++ src/main/scala/expr/Multiply.scala
@@ -0,0 +1,4 @@
+package cl.ravenhill.scrabble.dx
+package expr
+
+class Multiply(val left: Expression, val right: Expression) extends Expression
